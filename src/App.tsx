d414e3f0eb4f1260780e43a197c66367f04f4cd2
panduputragit <put.rariadi1144@gmail.com>
2024-04-21 18:01:18 +0800

    add function create todo

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,26 @@ function App() {
     );
   }
 
+  function addTodo(title:string){
+    setTodos(
+      (prevTodos => [
+        {
+          id: prevTodos.length+1,
+          title,
+          complete: false
+        },
+        ...prevTodos
+      ])
+    )
+  }
+
   return (
     <main className="py-10 space-y-5">
       <h1 className="font-bold text-3xl text-center">Your Todo Apps</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
-        <TodoForm />
+        <TodoForm 
+          onSubmit={addTodo}
+        />
         <div className="space-y-2">
           {/* memanggil dummy data yang telah di buat */}
           {todos.map(todo => (
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,8 +1,28 @@
-export default function TodoForm(){
+import { useState } from "react"
+
+//membuat props on submit
+interface AddTodoFormProps {
+    onSubmit: (title:string) => void;
+}
+
+export default function TodoForm({onSubmit}: AddTodoFormProps){
+    //controlled input
+    const [input, setInput] = useState("");
+
+    //function handle submit
+    function handelSubmitbutton(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        if (!input.trim()) return;
+
+        onSubmit(input);
+        setInput("");
+    }
     return(
-        <form action="" className="flex mb-2">
+        <form action="" className="flex mb-2" onSubmit={handelSubmitbutton}>
             <input 
-                type="text" 
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)} 
                 placeholder="apa yang seharusnya dilakukan?"
                 className="rounded-s-md  grow border border-gray-200 p-2"
             />
